aac5163407d56f40df7f692ee6885f8ec14536d6
esgore03 <143649079+esgore03@users.noreply.github.com>
2025-09-09 08:32:46 -0500

    package.json ajustments

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const userRoutes = require("./user.js");
 const taskRoutes = require("./task.js");
 const authRoutes = require("./auth.js");
-
+const API_URL = NEXT_PUBLIC_API_URL.env;
 const router = express.Router();
 
 router.use("/users", userRoutes);
