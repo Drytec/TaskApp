7cec5d3e2792d898d8c88b5213e6fb700fff3a9f
dylan <dylanfer830@gmail.com>
2025-09-08 20:22:07 -0500

    task show fixed

--- backend/routes/task.js
+++ backend/routes/task.js
@@ -3,9 +3,9 @@ const TaskController = require("../controllers/task");
 const auth = require("../middleware/auth");
 const router = express.Router();
 
-router.get("/", auth,(req, res) => TaskController.getAll(req, res));
+router.get("/", auth,(req, res) => TaskController.getAllTasks(req, res));
 router.get("/:id",auth, (req, res) => TaskController.read(req, res));
-router.post("/",auth, (req, res) => TaskController.create(req, res));
+router.post("/",auth, (req, res) => TaskController.createTask(req, res));
 router.put("/:id", auth,(req, res) => TaskController.update(req, res));
 router.delete("/:id", auth,(req, res) => TaskController.delete(req, res));
 
